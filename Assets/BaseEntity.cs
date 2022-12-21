d8e5e280eb8540fb1ca87ca2e9151e2bb808f824
Colorlessqq <115871291+Colorlessqq@users.noreply.github.com>
2022-12-22 01:28:23 +0300

    Oyun

---
+++ Assets/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntity : MonoBehaviour
+{
+    [SerializeField]
+    bool IsEnemy;
+    private void OnMouseDown()
+    {
+        // oyuncu karaterin üzerine bastığı zaman çalışır
+        // ToDo: yapılabilcek hamleleri gösterir
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
